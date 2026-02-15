8052e0d62d70b870655968876006f69eca9c4a18
pankaj_421 <pankajkr12022001@gmail.com>
2026-02-15 11:30:09 +0530

    implementing pagination

--- my-app/src/Pages/Test/Test.tsx
+++ my-app/src/Pages/Test/Test.tsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import styles from './Test.module.css';
+import Pagination from '../../components/pagination/pagination';
 
 function TestComponent() {
-  return <div className={styles['container']}>this is an test component</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const changePage = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div className={styles['container']}>
+      <Pagination
+        totalItem={25}
+        currentItem={currentPage}
+        changePage={changePage}
+      />
+    </div>
+  );
 }
 
 export default TestComponent;
--- my-app/src/components/pagination/pagination.tsx
+++ my-app/src/components/pagination/pagination.tsx
@@ -1,31 +1,97 @@
+import React from 'react';
+import styles from './pagination.module.css';
 
-import React from "react";
-import styles from "./pagination.module.css";
+type PaginationProps = {
+  totalItem: number;
+  currentItem: number;
+  changePage: (page: number) => void;
+};
 
-type paginationProps = {
-    totalItem: number,
-    currentItem: number,
-    changePage: (page: number) => void
-}
+const Pagination: React.FC<PaginationProps> = ({
+  totalItem,
+  currentItem,
+  changePage,
+}) => {
+  const getPages = () => {
+    const pages: (number | string)[] = [];
 
-const Pagination: React.FC<paginationProps> = ({ totalItem, currentItem, changePage }) => {
-    return (
-        <div className={styles["main-cnt"]}>
-            {
-                Array.from({ length: totalItem }, (_, idx) => idx + 1).map((item, index) => {
-                    return (
-                        <div
-                            onClick={() => changePage(item)}
-                            className={`${styles.tab} ${currentItem === item ? styles["active-tab"] : ""}`}
-                        >
-                            {item}
-                        </div>
+    if (totalItem <= 7) {
+      return Array.from({ length: totalItem }, (_, i) => i + 1);
+    }
 
-                    )
-                })
-            }
-        </div>
-    )
-}
+    pages.push(1);
+
+    if (currentItem > 4) {
+      pages.push('...');
+    }
+
+    const start = Math.max(2, currentItem - 1);
+    const end = Math.min(totalItem - 1, currentItem + 1);
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    if (currentItem < totalItem - 3) {
+      pages.push('...');
+    }
+
+    pages.push(totalItem);
+
+    return pages;
+  };
+
+  const pages = getPages();
+
+  const handlePrev = () => {
+    if (currentItem > 1) {
+      changePage(currentItem - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentItem < totalItem) {
+      changePage(currentItem + 1);
+    }
+  };
+
+  return (
+    <div className={styles['main-cnt']}>
+      <button
+        onClick={handlePrev}
+        disabled={currentItem === 1}
+        className={styles.navBtn}
+      >
+        Prev
+      </button>
+
+      {pages.map((item, index) =>
+        item === '...' ? (
+          <span key={`dots-${index}`} className={styles.dots}>
+            ...
+          </span>
+        ) : (
+          <div
+            key={item}
+            onClick={() => changePage(item as number)}
+            className={`${styles.tab} ${
+              currentItem === item ? styles['active-tab'] : ''
+            }`}
+          >
+            {item}
+          </div>
+        )
+      )}
+
+      <button
+        onClick={handleNext}
+        disabled={currentItem === totalItem}
+        className={styles.navBtn}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Pagination;
